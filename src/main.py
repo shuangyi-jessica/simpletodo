96560b4e75ea81f044cd476c44eb45cf844e79c7
Jessica Liu <t-jessicaliu@microsoft.com>
2023-08-18 14:10:57 +0200

    todolist

---
+++ src/main.py
@@ -0,0 +1,75 @@
+﻿print("Hello, World!")
+
+from typing import Union
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+
+app = FastAPI() # create a fastAPI instance
+MYTODOLIST = []
+USERS = []
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World2"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item.price = 0
+    return item
+
+class ToDo(BaseModel):
+    name: str
+    description: str | None = None
+    assignto: str | None = None
+
+@app.get("/todos/")
+def get_todoList():
+    return MYTODOLIST
+
+@app.post("/todos/")
+def add_new_todo(todo:ToDo):
+    if todo.assignto:
+        if verify_user_exists(todo.assignto):
+            MYTODOLIST.append(todo)
+        else:
+            raise HTTPException(status_code=400,detail="user not found",headers={"X-Error": "There goes my error"}, )
+    else:
+        MYTODOLIST.append(todo)
+    return todo
+
+class User(BaseModel):
+    name: str
+    username: str
+    password: str
+
+@app.get("/usernames/")
+def list_usernames():
+    usernamelist = []
+    for username in USERS:
+        usernamelist.append(username.name)
+    return usernamelist
+
+@app.post("/usernames/")
+def add_user(user:User):
+    USERS.append(user)
+    return user.name
+
+def verify_user_exists(name):
+    for user in USERS:
+        if user.username == name:
+            return True
+    return False
+
---
+++ src/practice.py
@@ -0,0 +1,73 @@
+﻿# # Create a sub-class
+# from enum import Enum
+
+# from fastapi import FastAPI
+
+
+# class ModelName(str, Enum):
+#     alexnet = "alexnet"
+#     resnet = "resnet"
+#     lenet = "lenet"
+
+
+# app = FastAPI()
+
+
+# @app.get("/models/{model_name}")
+# async def get_model(model_name: ModelName): # declare a path parameter
+#     if model_name is ModelName.alexnet: # compare enumeration members
+#         return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+#     if model_name.value == "lenet": # get the enumeration value
+#         return {"model_name": model_name, "message": "LeCNN all the images"}
+
+#     return {"model_name": model_name, "message": "Have some residuals"}
+
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+# @app.get("/items/")
+# async def read_item(skip: int = 0, limit: int = 10): # query parameters with default values: http://127.0.0.1:8000/items/?skip=0&limit=10
+#     return fake_items_db[skip : skip + limit]
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: str | None = None):
+#     if q:
+#         return {"item_id": item_id, "q": q}
+#     return {"item_id": item_id}
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: str | None = None, short: bool = False):
+#     item = {"item_id": item_id}
+#     if q:
+#         item.update({"q": q})
+#     if not short:
+#         item.update(
+#             {"description": "This is an amazing item that has a long description"}
+#         )
+#     return item
+
+# Create your data model
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+
